c039e16e5cfb50113d2faf1e35d0b2ffa5a0641b
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-10-30 00:22:20 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+import os
+import io
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='f.',description="Selfbot Made By Shadow Tech™ Company Owner: Free TNT#5796\n\nHelp Commands", self_bot=True)
+# IMPORTANT!
+# Change The Prefix f. To Anything You Want
+# Change Owner ID To Your ID 
+
+@bot.event()
+async def on_ready
+    print("Selfbot Online And Succesfully Installed")
+    print("_________________________")
+    print("Creator: Free TNT#5796")
+    
+@bot.command()
+async def Ping
+    """Pong! Check If Bot Is Working"""
+    await ctx.send("Pong!")
