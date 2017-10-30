4c7b891c1523c5d9c2bd2b55a9f9c423ac89c06a
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-10-31 02:18:28 +0300

    Indentions

--- bot.py
+++ bot.py
@@ -20,76 +20,76 @@ class Selfbot(commands.Bot):
 
     _mention_pattern = re.compile('|'.join(_mentions_transforms.keys()))
 
-def __init__(self, **attrs):
-        super().__init__(command_prefix=self.get_pre, self_bot=True)
-        self.session = aiohttp.ClientSession(loop=self.loop)
-        self.process = psutil.Process()
-        self._extensions = [x.replace('.py', '') for x in os.listdir('cogs') if x.endswith('.py')]
-        self.last_message = None
-        self.messages_sent = 0
-        self.commands_used = defaultdict(int)
-        self._add_commands()
-        self.load_extensions()
+    def __init__(self, **attrs):
+            super().__init__(command_prefix=self.get_pre, self_bot=True)
+            self.session = aiohttp.ClientSession(loop=self.loop)
+            self.process = psutil.Process()
+            self._extensions = [x.replace('.py', '') for x in os.listdir('cogs') if x.endswith('.py')]
+            self.last_message = None
+            self.messages_sent = 0
+            self.commands_used = defaultdict(int)
+            self._add_commands()
+            self.load_extensions()
 
-def _add_commands(self):
-    '''Adds commands automatically'''
-    for attr in dir(self):
-        cmd = getattr(self, attr)
-        if isinstance(cmd, commands.Command):
-            self.add_command(cmd)
+    def _add_commands(self):
+        '''Adds commands automatically'''
+        for attr in dir(self):
+            cmd = getattr(self, attr)
+            if isinstance(cmd, commands.Command):
+                self.add_command(cmd)
 
-def load_extensions(self, cogs=None, path='cogs.'):
-    '''Loads the default set of extensions or a separate one if given'''
-    for extension in cogs or self._extensions:
+    def load_extensions(self, cogs=None, path='cogs.'):
+        '''Loads the default set of extensions or a separate one if given'''
+        for extension in cogs or self._extensions:
+            try:
+                self.load_extension(f'{path}{extension}')
+                print(f'Loaded extension: {extension}')
+            except Exception as e:
+                print(f'LoadError: {extension}\n'
+                      f'type(e).__name__: {e}')
+
+
+    async def on_ready(self):
+        print("Selfbot Online And Succesfully Installed")
+        print("_________________________")
+        print("Creator: Free TNT#5796")
+    
+    @property
+    def token():
+        '''Returns your token wherever it is'''
+        with open('data/config.json') as f:
+            config = json.load(f)
+            if config.get('TOKEN') == "your_token_here":
+                if not os.environ.get('TOKEN'):
+                    self.run_wizard()
+            else:
+                token = config.get('TOKEN').strip('\"')
+        return os.environ.get('TOKEN') or token
+
+    @classmethod
+    def init(bot, token=None):
+        '''Starts the actual selfbot'''
+        selfbot = bot()
+        safe_token = token or selfbot.token.strip('\"')
         try:
-            self.load_extension(f'{path}{extension}')
-            print(f'Loaded extension: {extension}')
+            selfbot.run(safe_token, bot=False, reconnect=True)
         except Exception as e:
-            print(f'LoadError: {extension}\n'
-                  f'type(e).__name__: {e}')
+            print(e)
 
+    @staticmethod
+    async def get_pre(bot, message):
+        '''Returns the prefix'''
+        with open('data/config.json') as f:
+            prefix = json.load(f).get('PREFIX')
+        return os.environ.get('PREFIX') or prefix or 'f.'
 
-async def on_ready():
-    print("Selfbot Online And Succesfully Installed")
-    print("_________________________")
-    print("Creator: Free TNT#5796")
+    @commands.command()
+    async def Ping(self, ctx):
+        """Pong! Check If Bot Is Working"""
+        await ctx.send("Pong!")
     
-@property
-def token():
-    '''Returns your token wherever it is'''
-    with open('data/config.json') as f:
-        config = json.load(f)
-        if config.get('TOKEN') == "your_token_here":
-            if not os.environ.get('TOKEN'):
-                self.run_wizard()
-        else:
-            token = config.get('TOKEN').strip('\"')
-    return os.environ.get('TOKEN') or token
-
-@classmethod
-def init(bot, token=None):
-    '''Starts the actual selfbot'''
-    selfbot = bot()
-    safe_token = token or selfbot.token.strip('\"')
-    try:
-        selfbot.run(safe_token, bot=False, reconnect=True)
-    except Exception as e:
-        print(e)
-
-@staticmethod
-async def get_pre(bot, message):
-    '''Returns the prefix'''
-    with open('data/config.json') as f:
-        prefix = json.load(f).get('PREFIX')
-    return os.environ.get('PREFIX') or prefix or 'f.'
-
-@commands.command()
-async def Ping(self, ctx):
-    """Pong! Check If Bot Is Working"""
-    await ctx.send("Pong!")
-    
-if __name__ == '__main__':
-    Selfbot.init()
+    if __name__ == '__main__':
+        Selfbot.init()
 
 # if not os.environ.get('TOKEN'):
 #   print("no token found REEEE!")
